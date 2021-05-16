c26ad6f6b9289c64e315375e3d33f570a2c420d0
zonaliao <liaozona@gmail.com>
2021-05-15 21:03:43 -0700

    csv to json convert

--- Week7 Project/js/map.js
+++ Week7 Project/js/map.js
@@ -2,11 +2,8 @@
 let map;
 let lat = 0;
 let lon = 0;
-let zl = 18;
-let path = "data/DataMerge_2017.csv";
-let markers = L.featureGroup();
-let highpollution_markers = L.featureGroup();
-let lowpollution_markers = L.featureGroup();
+let zl = 3;
+let path = '';
 
 // put this in your global variables
 let geojsonPath = 'data/world.json';
@@ -22,9 +19,8 @@ let info_panel = L.control();
 
 // initialize
 $( document ).ready(function() {
-    createMap(lat,lon,zl);
-    readCSV(path);
-    getGeoJSON(); // read JSON file
+	createMap(lat,lon,zl);
+	getGeoJSON();
 });
 
 // create the map
@@ -96,112 +92,6 @@ function getStyle(feature){
 		fillOpacity: 0.8
 	}
 }
-
-
-// function to read csv data
-function readCSV(path){
-	Papa.parse(path, {
-		header: true,
-		download: true,
-		complete: function(data) {
-			console.log(data);
-			
-			// map the data
-			mapCSV(data);
-
-		}
-	});
-}
-
-function mapCSV(data){
-
-    let circleOptionsHigh = {
-        radius: 5,
-        weight: 1,
-        color: 'white',
-        fillColor: '#FF6962',
-        fillOpacity: 1,
-        //radius: item['Outdoor.air.pollution..IHME..2019.']*100
-    }
-
-    let circleOptionsLow = {
-        radius: 5,
-        weight: 1,
-        color: 'white',
-        fillColor: '#5EA777',
-        fillOpacity: 1,
-        //radius: item['Outdoor.air.pollution..IHME..2019.']*100
-    }
-
-    $(".sidebar").append(`<div class = "sidebar-item" onclick = "map.flyTo([25.930414, 50.637772], 5)">Bahrain</div>`)
-    $(".sidebar").append(`<div class = "sidebar-item" onclick = "map.flyTo([26.820553, 30.802498], 5)">Egypt</div>`)
-    $(".sidebar").append(`<div class = "sidebar-item" onclick = "map.flyTo([29.31166, 47.481766], 5)">Kuwait</div>`)
-    $(".sidebar").append(`<div class = "sidebar-item" onclick = "map.flyTo([38.963745, 35.243322], 5)">Turkey</div>`)
-    $(".sidebar").append(`<div class = "sidebar-item" onclick = "map.flyTo([35.86166, 104.195397], 5)">China</div>`)
-
-
-data.data.forEach(function(item,index){
-    if(item['Outdoor.air.pollution..IHME..2019.'] > 6.00){
-        //circleOptions.radius = item['Outdoor.air.pollution..IHME..2019.'] * 100
-        //circleOptions.fillColor = 'red'
-        let highpollution_marker = L.circleMarker([item.latitude,item.longitude], circleOptionsHigh).bindPopup(`${item.country}<br> Percentage of Deaths due to Pollution: ${item['Outdoor.air.pollution..IHME..2019.']}`).on('mouseover',function(){
-            this.openPopup()
-    })
-
-    highpollution_markers.addLayer(highpollution_marker)
-
-    //add entry to sidebar
-    //$('.sidebar').append(`<img src="${item.thumbnail_url}" onmouseover="panToImage(${index})">`)
-}
-
-else{
-    //circleOptions.radius = item['Outdoor.air.pollution..IHME..2019.'] * 100
-    //circleOptions.fillColor = 'green'
-    let lowpollution_marker = L.circleMarker([item.latitude,item.longitude], circleOptionsLow).bindPopup(`${item.country}<br> Percentage of Deaths due to Pollution: ${item['Outdoor.air.pollution..IHME..2019.']}`).on('mouseover',function(){
-        this.openPopup()
-})
-
-lowpollution_markers.addLayer(lowpollution_marker)
-
-}
-
-highpollution_markers.addTo(map);
-lowpollution_markers.addTo(map);
-
-
-})
-
-let addLayers = {
-    "High Death Rates": highpollution_markers,
-    "Low Death Rates": lowpollution_markers,
-}
-
-L.control.layers(null,addLayers).addTo(map);
-
-map.fitBounds(lowpollution_markers.getBounds());
-
-}
-
-
-
-
-function flyToIndex(index){
-    map.setZoom(5);
-    map.panTo(lowpollution_markers.getLayers()[index].getLatLng());
-    //markers.getLayers()[index].openPopup()
-}
-
-
-/*
-function panToImage(index){
-	// zoom to level 17 first
-	map.setZoom(17);
-	// pan to the marker
-	map.panTo(markers.getLayers()[index]._latlng);
-
-}
-*/
-
 
 // return the color for each feature
 function getColor(d) {
@@ -302,4 +192,3 @@ function createInfoPanel(){
 
 	info_panel.addTo(map);
 }
-
